756d6672b4eea8ecc3801276efdd28ecf6caaa73
Usirusen Ikon <lexicon048@gmail.com>
2023-06-22 12:45:28 +0100

    Create hamburger menu and implement funtionality

--- src/routes/landingpage/components/navigation/navigation.tsx
+++ src/routes/landingpage/components/navigation/navigation.tsx
@@ -1,19 +1,37 @@
-import logo from "../../../../assets/h.s/Logo.svg"
+import { Link } from "react-router-dom";
+import logo from "../../../../assets/h.s/Logo.svg";
+import Button from "../reusable/button";
+import styles from "./navigation.module.css";
+import { useState } from "react";
+import arrow from "../../../../assets/h.s/arrow.svg";
 //type Props = {}
 
 function Navigation() {
+  const [open, setopen] = useState(false);
+  const onClickHandler = () => {
+    setopen(!open);
+  };
+
   return (
     <div>
       <div>
         <img src={logo} alt="logo" />
         <span>Kaban</span>
       </div>
-      <ul>
-        <li><a href="">Property</a></li>
-        <li><a href="">Realtors</a></li>
-        <li><a href="">Company</a></li>
-        <li><a href="">Blog</a></li>
-        <li><button>Register</button></li>
+      <Button
+        onClick={onClickHandler}
+        className={`flex ${styles.hamburger_menu}`}
+        >
+          <span className={`${styles.line} ${open ? styles.tilt : styles.tilt_1}`}></span>
+          <span className={`${styles.line} ${open ? styles.hide : ''}`}></span>
+          <span className={`${styles.line} ${open ? styles.rtilt : styles.rtilt_1}`}></span>
+      </Button>
+      <ul className={`flex ${styles.nav_content} ${open ? styles.open : styles.close}`}>
+        <li className="flex"><a href="">Property</a><img src={arrow} alt="arrow" /></li>
+        <li className="flex"><Link to="">Realtors</Link><img src={arrow} alt="arrow" /></li>
+        <li className="flex"><Link to="">Company</Link><img src={arrow} alt="arrow" /></li>
+        <li className="flex"><Link to="">Blog</Link></li>
+        <li className={`flex ${styles.reg}`}><Button className={styles.reg_btn}>Register</Button></li>
       </ul>
     </div>
   )
---
+++ src/routes/landingpage/components/reusable/button.tsx
@@ -0,0 +1,15 @@
+import { ReactNode } from "react";
+import "../../../../main.css";
+
+type Props = {
+  className: string,
+  children: ReactNode,
+} & React.ComponentProps<'button'>
+
+export default function Button({className, children, ...buttonProps}: Props) {
+  return (
+    <button className={`btn ${className}`} {...buttonProps}>
+      {children}
+    </button>
+  )
+}
