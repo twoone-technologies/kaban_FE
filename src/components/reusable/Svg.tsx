5fb27b1520358836ddb7fd350f3a19219176372f
Usirusen Ikon <lexicon048@gmail.com>
2023-06-28 16:07:58 +0100

    nav-update

--- src/components/reusable/Svg.tsx
+++ src/components/reusable/Svg.tsx
@@ -9,7 +9,7 @@ type Props = {
 export default function Svg ({className, width='1.5em', height='1.5em', width_2='100%', href}: Props) {
   return (
     <svg 
-      className={`svg_icon ${className}`} 
+      className={`svg_icon ${className}`}
       width={width} 
       height={height}>
       <use width={width_2} href={href}></use>
