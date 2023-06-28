15b0688b1b969ceb4aeeea09e29192a20c79bd9e
Usirusen Ikon <lexicon048@gmail.com>
2023-06-28 10:00:09 +0100

    Render Navigation component in root

--- src/routes/_index.tsx
+++ src/routes/_index.tsx
@@ -1,6 +1,12 @@
 import { Outlet } from "react-router-dom";
+import Navigation from "~/components/navigation/Navigation";
 import "~/styles/main.css";
 
 export default function Root() {
-    return <Outlet />
+  return (
+    <>
+      <Navigation />
+      <Outlet />
+    </>
+  )
 }
