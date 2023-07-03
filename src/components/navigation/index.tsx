1b67560e223e937e7e02fa4b26a061a66708f162
Usirusen Ikon <lexicon048@gmail.com>
2023-07-03 14:10:02 +0100

    Update color on navbar and navitems on-scroll and in diffrent routes

--- src/components/navigation/index.tsx
+++ src/components/navigation/index.tsx
@@ -1,49 +1,102 @@
 import { useState } from "react";
+import { Link, useLocation } from "react-router-dom";
 import { logoIcon } from "~/assets";
 import Button from "~/components/reusable/Button";
 import styles from "./navigation.module.css";
-import navbarData from "../data/navbarData";
+import navbarData from "./navbarData";
 import Svg from "~/components/reusable/Svg";
 import NavItem from "./navitem";
 
 function Navigation() {
-  const [open, setopen] = useState(false);
-  const [dropdown, setDropdown] = useState(-1);
+  const [open, setOpen] = useState(false);
+  const [dropDown, setDropDown] = useState(-1);
+  const [navBar, setNavBar] = useState(false);
+
+  const colorSwap = () => {
+    window.scrollY >= 70 ? setNavBar(true) : setNavBar(false)
+  };
+  window.addEventListener('scroll', colorSwap);
+
+  const location = useLocation();
 
   const onClickHandler = () => {
-    setopen(!open);
+    setOpen(!open);
   };
 
-  return (
-    <nav className={`flex f-width ${styles.nav}`}>
-      <Svg href={logoIcon} width="100px" height="40px" className={styles.logo}/>
-      <Button
-        type="button"
-        onClick={onClickHandler}
-        className={`flex f-column ${styles.hamburger_menu} ${open ? styles.border : styles.border_1}`}
+  let content;
+
+  location.pathname === "/" ?
+    content = (
+      <nav className={`flex f-width ${styles.nav} 
+      ${navBar ? `bg-tetiary secondary-color` : ''}`}>
+      <Link to={"/"}>
+        <Svg href={logoIcon} width="100px" height="40px"
+          className={`${navBar ? `primary-color` : styles.logo}`} />
+      </Link><Button type="button" title="menu" onClick={onClickHandler}
+        className={`flex f-column ${styles.hamburger_menu}
+        ${open || navBar ? styles.border : styles.border_1}`}
       >
-        <span className={`${styles.line} ${open ? styles.tilt : styles.tilt_1}`}></span>
-        <span className={`${styles.line} ${open ? styles.hide : styles.see}`}></span>
-        <span className={`${styles.line} ${open ? styles.rtilt : styles.rtilt_1}`}></span>
-      </Button>
-      <ul className={`flex ${styles.nav_content} ${open ? styles.open : styles.close}`}>
-        {Object.entries(navbarData).map(([key, val], idx) => (
-          <NavItem key={key} 
-            title={key} 
-            href={val.href} 
-            subItems={val.subItems} 
-            drop={dropdown === idx} 
-            handleClick={() => setDropdown(prev => prev === idx ? -1 : idx)}
-            mouseOver={() => setDropdown(prev => prev === idx ? -1 : idx)} />
-        ))}
-        <li className={`flex ${styles.reg}`}>
-          <Button type="submit"
-            className={styles.reg_btn}>
-            Register
-          </Button>
-        </li>
-      </ul>
-    </nav>
+          <span className={`${styles.line} ${open ? styles.tilt : ''} 
+        ${navBar || open ? styles.hue_1 : styles.hue_2}`}></span>
+          <span className={`${styles.line} ${open ? styles.hide : styles.see} 
+        ${navBar || open ? styles.hue_1 : styles.hue_2}`}></span>
+          <span className={`${styles.line} ${open ? styles.rtilt : ''} 
+        ${navBar || open ? styles.hue_1 : styles.hue_2}`}></span>
+        </Button><ul className={`flex ${styles.nav_content} ${open ? styles.open : styles.close}`}>
+          {Object.entries(navbarData).map(([key, val], idx) => (
+            <NavItem key={key}
+              title={key}
+              href={val.href}
+              subItems={val.subItems}
+              drop={dropDown === idx}
+              handleClick={() => setDropDown(prev => prev === idx ? -1 : idx)}
+              mouseOver={() => setDropDown(prev => prev === idx ? -1 : idx)}
+              navState={navBar} />
+          ))}
+          <li className={`flex ${styles.reg}`}>
+            <Button type="submit"
+              className={styles.reg_btn}>
+              Register
+            </Button>
+          </li>
+        </ul>
+        </nav>
+    ) :
+    content = (
+      <nav className={`flex f-width bg-tetiary secondary-color ${styles.nav}`}>
+        <Link to={"/"}>
+          <Svg href={logoIcon} width="100px" height="40px"
+            className='primary-color' />
+        </Link>
+        <Button type="button" title="menu" onClick={onClickHandler}
+          className={`flex f-column ${styles.hamburger_menu} ${styles.border}`}>
+          <span className={`${styles.line} ${open ? styles.tilt : ''} ${styles.hue_1}`}></span>
+          <span className={`${styles.line} ${open ? styles.hide : styles.see} ${styles.hue_1}`}></span>
+          <span className={`${styles.line} ${open ? styles.rtilt : ''} ${styles.hue_1}`}></span>
+        </Button>
+        <ul className={`flex ${styles.nav_content} ${open ? styles.open : styles.close}`}>
+          {Object.entries(navbarData).map(([key, val], idx) => (
+            <NavItem key={key}
+              title={key}
+              href={val.href}
+              subItems={val.subItems}
+              drop={dropDown === idx}
+              handleClick={() => setDropDown(prev => prev === idx ? -1 : idx)}
+              mouseOver={() => setDropDown(prev => prev === idx ? -1 : idx)}
+              navState={true} />
+          ))}
+          <li className={`flex ${styles.reg}`}>
+            <Button type="submit"
+              className={styles.reg_btn}>
+              Register
+            </Button>
+          </li>
+        </ul>
+        </nav>
+    )
+
+  return (
+    <>{content}</>
   )
 }
 
