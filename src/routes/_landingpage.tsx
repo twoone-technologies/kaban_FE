058218cdfd3739ad4543063ca5345b8b127c367a
Kingsley <kingsleyakwa@gmail.com>
2023-07-12 18:05:33 +0100

    feat: added property_type section

--- src/routes/_landingpage.tsx
+++ src/routes/_landingpage.tsx
@@ -1,9 +1,11 @@
+import PropertyTypeSection from "~/components/propertyTypeSection/PropertyTypeSection";
 import HeroSection from "../components/heroSection/HeroSection";
 
 export default function LandingPage() {
   return (
     <>
       <HeroSection />
+      <PropertyTypeSection />
     </>
   )
 } 
