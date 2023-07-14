2b6d822e2fbe1c100340d018f6a3f07ad1fe75f6
Kingsley <kingsleyakwa@gmail.com>
2023-07-14 10:47:09 +0100

    feat: created explore_cities-section

---
+++ src/components/data/cityCardData.ts
@@ -0,0 +1,22 @@
+export const cities = [
+    {
+        name: 'Port Harcourt',
+        slogan: 'The treasure base',
+        image: "/src/assets/img/city-of-uyo.jpg",
+    },
+    {
+        name: 'Asaba',
+        slogan: 'The big heart',
+        image: "/src/assets/img/city-of-uyo.jpg",
+    },
+    {
+        name: 'Calabar',
+        slogan: "The Nation's paradise",
+        image: "/src/assets/img/city-of-uyo.jpg",
+    },
+    {
+        name: 'Uyo',
+        slogan: 'The land of promise',
+        image: "/src/assets/img/city-of-uyo.jpg",
+    },
+]
--- src/routes/_landingpage.tsx
+++ src/routes/_landingpage.tsx
@@ -1,11 +1,13 @@
 import PropertyTypeSection from "~/components/propertyTypeSection/PropertyTypeSection";
 import HeroSection from "../components/heroSection/HeroSection";
+import ExploreCitiesSection from "~/components/exploreCitiesSection/ExploreCitiesSection";
 
 export default function LandingPage() {
   return (
     <>
       <HeroSection />
-      <PropertyTypeSection />
+      {/* <PropertyTypeSection /> */}
+      <ExploreCitiesSection />
     </>
   )
 } 
