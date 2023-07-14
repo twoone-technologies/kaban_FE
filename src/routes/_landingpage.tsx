34fb716d7709058a5863800c99ae3198d4478898
Kingsley <kingsleyakwa@gmail.com>
2023-07-14 11:22:45 +0100

    Removed unrelated code

--- src/routes/_landingpage.tsx
+++ src/routes/_landingpage.tsx
@@ -1,12 +1,10 @@
-import PropertyTypeSection from "~/components/propertyTypeSection/PropertyTypeSection";
-import HeroSection from "../components/heroSection/HeroSection";
+import HeroSection from "~/components/herosection/HeroSection";
 import ExploreCitiesSection from "~/components/exploreCitiesSection/ExploreCitiesSection";
 
 export default function LandingPage() {
   return (
     <>
       <HeroSection />
-      {/* <PropertyTypeSection /> */}
       <ExploreCitiesSection />
     </>
   )
