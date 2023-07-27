2e061fd719a7fbc6e377a8650595dda69b2b63fd
Kingsley <kingsleyakwa@gmail.com>
2023-07-27 21:28:08 +0100

    changed the section-heading co

--- src/components/propertyTypeSection/PropertyTypeSection.tsx
+++ src/components/propertyTypeSection/PropertyTypeSection.tsx
@@ -7,7 +7,7 @@ export default function PropertyTypeSection() {
     <section className="section">
       <Container element='div'>
         <div className={`pad ${style.section_title}`}>
-          <h1 className='section_heading'>Property Types</h1>
+          <h2>Property Types</h2>
           <p>Explore properties listings nationwide under this categories</p>
         </div>
         <div className={style.grid_container}>
