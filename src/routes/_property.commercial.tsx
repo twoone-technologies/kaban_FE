d9fc4f4fa388f5568b1671837c5272e0c1d888bb
Usirusen Ikon <lexicon048@gmail.com>
2023-10-25 10:40:33 +0100

    Update Components with useHeader hook and resultsWrap component

--- src/routes/_property.commercial.tsx
+++ src/routes/_property.commercial.tsx
@@ -1,6 +1,48 @@
-export default function Commercial() {
-    return (
-      <div>Commercial</div>
-    )
+import { ActionFunctionArgs, redirect } from 'react-router-dom';
+import { dummyObj } from '~/components/reusable/dummyObj';
+import ResultsWrap from '~/components/reusable/resultsContainer/ResultsWrap';
+import useHeader from '~/hooks/useHeader';
+
+export async function action({ request }: ActionFunctionArgs) {
+  // get form data
+  const formData = await request.formData();
+  console.log(...formData);
+  const searchStr = [...formData]
+    .reduce((prev, [key, val]) => {
+      if (val) prev.push(`${key}=${val}`);
+      return prev;
+    }, [] as string[])
+    .join('&');
+  // const str = `search_results?status=${status}&location=${location}&propertyType=${propertyType}&bedrooms=${bedrooms}&priceRange=${priceRange}`
+  // console.log(str.split('&'));
+
+  // send data to BE
+  //const res = await searchListings(searchStr)
+
+  // redirect to result page with the data
+
+  if (location.pathname === '/commercial') {
+    return redirect(`/commercial?${searchStr}`);
   }
-  
+  return redirect(`/commercial?${searchStr}`);
+}
+
+export default function Commercial() {
+  const {category, cityStatus, stat} = useHeader();
+  const commercial = dummyObj.filter(
+    (items) => items.property_category === 'commercial',
+  );
+
+  return (
+    <ResultsWrap
+      status={stat}
+      propertyCategory={category}
+      object={commercial}
+      onSubmit={() => {
+        setTimeout(() => {
+          cityStatus();
+        }, 500);
+      }}
+    />
+  );
+}
--- src/routes/_property.industrial.tsx
+++ src/routes/_property.industrial.tsx
@@ -1,6 +1,44 @@
+import { ActionFunctionArgs, redirect } from 'react-router-dom';
+import { dummyObj } from '~/components/reusable/dummyObj';
+import ResultsWrap from '~/components/reusable/resultsContainer/ResultsWrap';
+import useHeader from '~/hooks/useHeader';
+
+export async function action({ request }: ActionFunctionArgs) {
+  // get form data
+  const formData = await request.formData();
+  console.log(...formData);
+  const searchStr = [...formData]
+    .reduce((prev, [key, val]) => {
+      if (val) prev.push(`${key}=${val}`);
+      return prev;
+    }, [] as string[])
+    .join('&');
+  // const str = `search_results?status=${status}&location=${location}&propertyType=${propertyType}&bedrooms=${bedrooms}&priceRange=${priceRange}`
+  // console.log(str.split('&'));
+
+  // send data to BE
+  //const res = await searchListings(searchStr)
+
+  // redirect to result page with the data
+
+  if (location.pathname === '/industrial') {
+    return redirect(`/industrial?${searchStr}`);
+  }
+  return redirect(`/industrial?${searchStr}`);
+}
 
 export default function Industrial() {
+  const { category, cityStatus, stat } = useHeader();
   return (
-    <div>Industrial</div>
-  )
+    <ResultsWrap
+      status={stat}
+      propertyCategory={category}
+      object={dummyObj}
+      onSubmit={() => {
+        setTimeout(() => {
+          cityStatus();
+        }, 500);
+      }}
+    />
+  );
 }
