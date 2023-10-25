37b1cbc5546a7090cad6a1db2868fe3b3a0eff2f
Usirusen Ikon <lexicon048@gmail.com>
2023-10-25 10:22:34 +0100

    Link Property category cards to  category pages

--- src/components/data/properyTypeData.ts
+++ src/components/data/properyTypeData.ts
@@ -2,16 +2,19 @@ export const propertyTypeData = [
     {
         name: 'commercial',
         type: ['Land', 'Office', 'Mall', 'Gas Station', 'School'],
-        image: '/src/assets/img/commercial.jpeg'
+        image: '/src/assets/img/commercial.jpeg',
+        link: 'commercial'
     },
     {
         name: 'industrial',
         type: ['Factory', 'Oil Rig', 'Power Plant', 'Warehouse'],
-        image: '/src/assets/img/industrial.jpeg'
+        image: '/src/assets/img/industrial.jpeg',
+        link: 'industrial'
     },
     {
         name: 'residential',
         type: ['Self Contain', 'Apartment', 'Detached House', 'Terace house', 'Semi-detached House'],
-        image: '/src/assets/img/residential.jpeg'
+        image: '/src/assets/img/residential.jpeg',
+        link: 'residential'
     }
 ]
