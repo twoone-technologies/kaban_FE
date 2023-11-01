08de975c51e97ff5d572a8c31dfd074549de1803
Usirusen Ikon <lexicon048@gmail.com>
2023-11-01 16:37:30 +0100

    Update useHeader hook

--- src/hooks/useHeader.ts
+++ src/hooks/useHeader.ts
@@ -10,24 +10,28 @@ export default function useHeader() {
   }
 
   const cityStatus = useCallback(() => {
-    if (location.pathname === '/cities/:cityName' && location.href.includes('location')) {
-      const url = new URL(location.href);
-      const urlParams = new URLSearchParams(document.location.search)
-      
-      const currCity = urlParams.get("location");
-
-      const new_params = new URLSearchParams([
-        ...Array.from(url.searchParams.entries()),
-      ]).toString();
-
-      const new_url = `${url.origin}/cities/${currCity}?${new_params}`;
-      window.history.pushState({ path: new_url }, '', new_url)
-    }
-    if (location.href.indexOf('?') === -1) {
+    if (location.href.includes('/cities')) {
       const key = location.href.split('/')[4];
       setCity(key.replace('%20', ' ') ?? '');
-    }
-    else if(location.pathname !== '/cities/:cityName') {console.log('piggy');
+
+      if (location.href.includes('location')) {
+        const url = new URL(location.href);
+        const urlParams = new URLSearchParams(document.location.search)
+        
+        const currCity = urlParams.get("location") || ''; 
+        const currStat = urlParams.get("status") || ''; 
+        setCity(currCity?.replace('%20', ' ') ?? '');
+        setStat(currStat)
+
+        const new_params = new URLSearchParams([
+          ...Array.from(url.searchParams.entries()),
+        ]).toString();
+  
+        const new_url = `${url.origin}/cities/${currCity}?${new_params}`;
+        window.history.pushState({ path: new_url }, '', new_url)
+        }
+      }
+    else if (location.pathname !== '/cities/:cityName' && location.href.includes('?')) {console.log('piggy');
       const keyValuePairs: string[] = location.href.split('?')[1].split('&');
       const resultArray: QueryObject[] = [];
       
