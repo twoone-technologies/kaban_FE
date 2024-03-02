08c98819897930ab3b10eea5824dfaa418225b4b
Usirusen Ikon <lexicon048@gmail.com>
2024-03-02 14:17:38 +0100

    Refactor Swiper to custom component

--- src/components/dashboard/postproperty/pages/media/Carousel.tsx
+++ src/components/dashboard/postproperty/pages/media/Carousel.tsx
@@ -2,13 +2,12 @@ import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/effect-coverflow';
-import { arrowIcon } from '~/assets/icons';
 import Svg from '~/components/reusable/Svg';
 import { coverStarIcon } from '~/assets/icons';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { EffectCoverflow, Pagination, Navigation } from 'swiper/modules';
+import { SwiperSlide } from 'swiper/react';
 import { ImageFile } from '~/components/dashboard/postproperty/pages/media';
 import styles from '~/components/dashboard/postproperty/pages/miscellenous/post.module.css';
+import CarouselWrap from '~/components/reusable/CarouselWrap';
 
 type Props = {
   imageArr: ImageFile[];
@@ -32,29 +31,11 @@ export default function Carousel({ imageArr, coverImage, deleteImage }: Props) {
         </div>
       ) : undefined}
       {imageArr.length > 0 ? (
-        <Swiper
-          grabCursor={true}
-          slidesPerView={'auto'}
-          loop={false}
-          updateOnWindowResize={true}
-          spaceBetween={28}
-          pagination={{ el: '.c-pagination', clickable: true }}
-          navigation={{
-            nextEl: '.btn-next',
-            prevEl: '.btn-prev',
-          }}
-          modules={[EffectCoverflow, Pagination, Navigation]}
-          breakpoints={{
-            600: {slidesPerView: 1, centeredSlides: true},
-            700: {slidesPerView: 2, centeredSlides: false},
-            1230: {slidesPerView: 3, centeredSlides: false},
-          }}
-          className="swiper_container"
-        >
+        <CarouselWrap>
           {imageArr.map((image, id) => (
             <SwiperSlide className="carousel_item" key={id}>
               <div className={styles.imgWrap}>
-                <img src={image.url} alt={image.name} className={styles.img} />
+                <img src={image.url} alt={image.name} className={`b-radius ${styles.img}`} />
                 <span onClick={() => deleteImage(id)}
                   className="flex align-x align-y absolute bottom-2 left-6"
                 >
@@ -63,18 +44,7 @@ export default function Carousel({ imageArr, coverImage, deleteImage }: Props) {
               </div>
             </SwiperSlide>
           ))}
-          <div className={`flex gap s-btw align-y carousel-btn-grp`}>
-            <div className="flex carousel-nav gap">
-              <div className={`carousel-btn flex align-y`}>
-                <Svg className="btn-prev" href={arrowIcon} />
-              </div>
-              <div className={'carousel-btn flex align-y'}>
-                <Svg className="btn-next" href={arrowIcon} />
-              </div>
-            </div>
-            <div className="flex gap c-pagination" />
-          </div>
-        </Swiper>
+        </CarouselWrap>
       ) : ('')}
     </div>
   );
--- src/components/featuredListings/FeaturedList.tsx
+++ src/components/featuredListings/FeaturedList.tsx
@@ -1,14 +1,12 @@
-import Card, { HouseCard } from "../reusable/card/Card";
-import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/effect-coverflow"
 import "swiper/css/pagination"
 import "swiper/css/navigation"
-import { EffectCoverflow, Pagination, Navigation } from "swiper/modules";
+import { SwiperSlide } from "swiper/react";
 import Container from "../reusable/Container";
 import { dummyObj } from "../reusable/dummyObj";
-import Svg from "../reusable/Svg";
-import { arrowIcon} from "~/assets/icons";
+import CarouselWrap from "../reusable/CarouselWrap";
+import Card, { HouseCard } from "../reusable/card/Card";
 
 export default function FeaturedList() {
   const arr = dummyObj as HouseCard[]
@@ -19,52 +17,13 @@ export default function FeaturedList() {
           <h2>Explore Our Featured Listings</h2>
           <p>Properties considered worth your<br /> time by listing realtors</p>
         </div>
-        <Swiper
-          grabCursor={true}
-          slidesPerView={'auto'}
-          centeredSlides={true}
-          loop={false}
-          updateOnWindowResize={true}
-          spaceBetween={28}
-          pagination={{el:'.c-pagination', clickable: true}}
-          navigation={{
-            nextEl: '.btn-next',
-            prevEl: '.btn-prev',
-          }}
-          modules={[EffectCoverflow, Pagination, Navigation]}
-          breakpoints={{
-            500: {
-              slidesPerView: 1,
-              centeredSlides: false,
-            },
-            700: {
-              slidesPerView: 2,
-              centeredSlides: false,
-            },
-            1180: {
-              slidesPerView: 3,
-              centeredSlides: false,
-            },
-          }}
-          className="swiper_container"
-        >
+        <CarouselWrap>
           {arr.map((item) => (
             <SwiperSlide className="carousel_item" key={item.id}>
               <Card card={item} />
             </SwiperSlide>
           ))}
-          <div className={`flex gap w-full align-y carousel-btn-grp`}>
-            <div className="flex carousel-nav gap">
-              <div className="carousel-btn flex align-y">
-                <Svg className="btn-prev" href={arrowIcon} />
-              </div>
-              <div className={"carousel-btn flex align-y"}>
-                <Svg className="btn-next" href={arrowIcon} />
-              </div>
-            </div>
-            <div className="flex gap c-pagination" />
-          </div>
-        </Swiper>
+        </CarouselWrap>
       </Container>
     </section>
   )
