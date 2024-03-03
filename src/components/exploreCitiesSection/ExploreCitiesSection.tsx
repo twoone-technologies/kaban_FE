e4ed1decbab9a5bd20098bebb0137fa5472a816a
Usirusen Ikon <lexicon048@gmail.com>
2024-03-03 19:53:37 +0100

    Convert property type and explore cities section to carousel on mobile

--- src/components/exploreCitiesSection/ExploreCitiesSection.tsx
+++ src/components/exploreCitiesSection/ExploreCitiesSection.tsx
@@ -1,12 +1,31 @@
 import { cities } from "../data/cityCardData"
+import { SwiperSlide } from 'swiper/react';
 import Container from "../reusable/Container"
 import CityCard from "./CityCard"
 import style from "./exploreCities.module.css"
+import useResponsiveNav from '~/hooks/useResponsiveNav';
+import CarouselWrap from "../reusable/CarouselWrap";
 
-export default function ExploreCitiesSection() {
+export default function ExploreCitiesSection() {  
+  const { mediaSize } = useResponsiveNav({});
   return (
     <section className="section bg-grey">
       <Container element="div">
+        {mediaSize === 'mobile' ? (
+          <div>
+            <div className={`flex f-column ${style.city_card_container}`}>
+              <h2>Explore Cities</h2>
+              <p>Have a look at listings located at some of the amazing cities in Nigeria.</p>
+            </div>
+            <CarouselWrap>
+              {cities.map(city => (
+                <SwiperSlide key={city.name}>
+                  <CityCard city={city} />
+                </SwiperSlide>
+              ))}
+            </CarouselWrap>
+          </div>
+        ) : (
         <div className={style.grid_container}>
           <div className={`flex f-column ${style.city_card_container}`}>
               <h2>Explore Cities</h2>
@@ -16,6 +35,7 @@ export default function ExploreCitiesSection() {
             <CityCard key={city.name} city={city} />
           ))}
         </div>
+        )}
       </Container>
     </section>
   )
--- src/components/propertyTypeSection/PropertyTypeSection.tsx
+++ src/components/propertyTypeSection/PropertyTypeSection.tsx
@@ -1,21 +1,36 @@
-import { propertyTypeData } from '../data/properyTypeData'
-import Container from '../reusable/Container'
-import PropertyTypeCard from './PropertyTypeCard'
-import style from "./property.module.css"
+import style from './property.module.css';
+import { SwiperSlide } from 'swiper/react';
+import Container from '../reusable/Container';
+import PropertyTypeCard from './PropertyTypeCard';
+import CarouselWrap from '../reusable/CarouselWrap';
+import useResponsiveNav from '~/hooks/useResponsiveNav';
+import { propertyTypeData } from '../data/properyTypeData';
+
 export default function PropertyTypeSection() {
+  const { mediaSize } = useResponsiveNav({});
   return (
     <section className="section">
-      <Container element='div'>
+      <Container element="div">
         <div className={`pad ${style.section_title}`}>
           <h2>Property Types</h2>
           <p>Explore properties listings nationwide under this categories</p>
         </div>
-        <div className={style.grid_container}>
-          {propertyTypeData.map(data => (
-            <PropertyTypeCard key={data.name} data={data} />
-          ))}
-        </div>
+        {mediaSize === 'mobile' ? (
+          <CarouselWrap>
+            {propertyTypeData.map((data) => (
+              <SwiperSlide key={data.name}>
+                <PropertyTypeCard data={data} />
+              </SwiperSlide>
+            ))}
+          </CarouselWrap>
+        ) : (
+          <div className={style.grid_container}>
+            {propertyTypeData.map((data) => (
+              <PropertyTypeCard key={data.name} data={data} />
+            ))}
+          </div>
+        )}
       </Container>
     </section>
-  )
+  );
 }
