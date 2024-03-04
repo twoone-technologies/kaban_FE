e12f4dafcf74257b298281bca0bdba125d11867c
Usirusen Ikon <lexicon048@gmail.com>
2024-03-04 10:04:29 +0100

    Update styles for forms and remove unecessary html tags

--- src/components/featuredListings/FeaturedList.tsx
+++ src/components/featuredListings/FeaturedList.tsx
@@ -1,30 +1,31 @@
-import "swiper/css";
-import "swiper/css/effect-coverflow"
-import "swiper/css/pagination"
-import "swiper/css/navigation"
-import { SwiperSlide } from "swiper/react";
-import Container from "../reusable/Container";
-import { dummyObj } from "../reusable/dummyObj";
-import CarouselWrap from "../reusable/CarouselWrap";
-import Card, { HouseCard } from "../reusable/card/Card";
+import 'swiper/css';
+import 'swiper/css/effect-coverflow';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import { SwiperSlide } from 'swiper/react';
+import Container from '../reusable/Container';
+import { dummyObj } from '../reusable/dummyObj';
+import CarouselWrap from '../reusable/CarouselWrap';
+import Card, { HouseCard } from '../reusable/card/Card';
 
 export default function FeaturedList() {
-  const arr = dummyObj as HouseCard[]
+  const arr = dummyObj as HouseCard[];
   return (
-    <section className="container-pad bg-grey">
-      <Container element="div">
-        <div className="text-align header">
-          <h2>Explore Our Featured Listings</h2>
-          <p>Properties considered worth your<br /> time by listing realtors</p>
-        </div>
-        <CarouselWrap>
-          {arr.map((item) => (
-            <SwiperSlide className="carousel_item" key={item.id}>
-              <Card card={item} />
-            </SwiperSlide>
-          ))}
-        </CarouselWrap>
-      </Container>
-    </section>
-  )
+    <Container element="div" className='container-pad'>
+      <div className="text-align header">
+        <h2>Explore Our Featured Listings</h2>
+        <p>
+          Properties considered worth your
+          <br /> time by listing realtors
+        </p>
+      </div>
+      <CarouselWrap>
+        {arr.map((item) => (
+          <SwiperSlide className="carousel_item" key={item.id}>
+            <Card card={item} />
+          </SwiperSlide>
+        ))}
+      </CarouselWrap>
+    </Container>
+  );
 }
--- src/components/propertyTypeSection/PropertyTypeSection.tsx
+++ src/components/propertyTypeSection/PropertyTypeSection.tsx
@@ -9,28 +9,26 @@ import { propertyTypeData } from '../data/properyTypeData';
 export default function PropertyTypeSection() {
   const { mediaSize } = useResponsiveNav({});
   return (
-    <section className="section">
-      <Container element="div">
-        <div className={`pad ${style.section_title}`}>
-          <h2>Property Types</h2>
-          <p>Explore properties listings nationwide under this categories</p>
+    <Container element="div" className='container-pad'>
+      <div className={style.section_title}>
+        <h2>Property Types</h2>
+        <p>Explore properties listings nationwide under this categories</p>
+      </div>
+      {mediaSize === 'mobile' ? (
+        <CarouselWrap>
+          {propertyTypeData.map((data) => (
+            <SwiperSlide key={data.name}>
+              <PropertyTypeCard data={data} />
+            </SwiperSlide>
+          ))}
+        </CarouselWrap>
+      ) : (
+        <div className={style.grid_container}>
+          {propertyTypeData.map((data) => (
+            <PropertyTypeCard key={data.name} data={data} />
+          ))}
         </div>
-        {mediaSize === 'mobile' ? (
-          <CarouselWrap>
-            {propertyTypeData.map((data) => (
-              <SwiperSlide key={data.name}>
-                <PropertyTypeCard data={data} />
-              </SwiperSlide>
-            ))}
-          </CarouselWrap>
-        ) : (
-          <div className={style.grid_container}>
-            {propertyTypeData.map((data) => (
-              <PropertyTypeCard key={data.name} data={data} />
-            ))}
-          </div>
-        )}
-      </Container>
-    </section>
+      )}
+    </Container>
   );
 }
