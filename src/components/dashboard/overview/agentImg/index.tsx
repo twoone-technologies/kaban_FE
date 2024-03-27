900ad0886f7b088a90deff929c2be32cb6c34b32
Usirusen Ikon <lexicon048@gmail.com>
2024-03-27 19:15:51 +0100

    Remove unused variables

--- src/components/dashboard/overview/agentImg/index.tsx
+++ src/components/dashboard/overview/agentImg/index.tsx
@@ -1,14 +1,11 @@
-import { useNavigate } from 'react-router-dom';
 import styles from './agentImg.module.css'
 import { editIcon } from '~/assets/icons';
 import ikon from '~/assets/img/Ikon.png';
 import Rating from '~/components/propertyItem/micellenous/Rating';
-import Button from '~/components/reusable/Button';
 import Svg from '~/components/reusable/Svg';
 import { Link } from 'react-router-dom';
 
 export default function AgentImg() {
-  const navigate = useNavigate()
   return (
     <div
       className={`flex f-column align-y pad-2 gap box_shadow ${styles.agentCard}`}
