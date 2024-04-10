ec20817ad8bcbca0815eaf5bf48ace07e3787fd3
gnobu <inyangubong17@gmail.com>
2024-04-10 19:33:19 +0100

    feat: persist auth token on refresh

--- src/api/store.ts
+++ src/api/store.ts
@@ -1,19 +1,29 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
 import { setupListeners } from '@reduxjs/toolkit/dist/query'
+import { persistStore, persistReducer } from 'redux-persist'
+// @ts-ignore
+import { CookieStorage } from 'redux-persist-cookie-storage'
+import Cookies from 'cookies-js'
 
 import { api } from './features/api'
 import authReducer from './slices/auth'
 
+const persistConfig = {
+    key: "kaban__session",
+    storage: new CookieStorage(Cookies)
+}
 
 export const store = configureStore({
-    reducer: {
+    reducer: combineReducers({
         [api.reducerPath]: api.reducer,
-        auth: authReducer,
-    },
+        auth: persistReducer(persistConfig, authReducer),
+    }),
     middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(api.middleware),
     devTools: true
 })
 setupListeners(store.dispatch)
+
+export let persistor = persistStore(store)
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import { Provider } from 'react-redux'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
+import { PersistGate } from 'redux-persist/integration/react'
+
+import { persistor, store } from './api/store'
 import { router } from './browserRouter'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <RouterProvider router={router} />
+      </PersistGate>
+    </Provider>
   </React.StrictMode>,
 )
