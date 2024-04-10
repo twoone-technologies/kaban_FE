215454a564cfa70daae18dc63474ed6bcd3a2e3c
Usirusen Ikon <lexicon048@gmail.com>
2024-04-10 09:09:48 +0100

    Remove console.log statement

--- src/routes/search_results.tsx
+++ src/routes/search_results.tsx
@@ -31,7 +31,6 @@ export async function action({ request }: ActionFunctionArgs) {
 
 export default function SearchResults() {
   const { city, cityStatus, stat } = useHeader();
-  console.log(city, stat);
   const arr = dummyObj as unknown as HouseCard[]
 
   return (
