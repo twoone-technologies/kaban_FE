bac421815228110e7d2ad67776c428bcc749e96d
Usirusen Ikon <lexicon048@gmail.com>
2024-05-23 14:15:18 +0100

    Update component to send socials object as input value

--- src/components/dashboard/editprofile/pages/SocialMedia.tsx
+++ src/components/dashboard/editprofile/pages/SocialMedia.tsx
@@ -2,12 +2,14 @@ import FormControl from '~/components/reusable/FormControl';
 import InputWrap from '../../reusables/InputWrap';
 import { UseFormRegister } from 'react-hook-form';
 import { EditProfileInputs } from '..';
+import { useState } from 'react';
 
 export default function SocialMedia({
   register,
 }: {
   register: UseFormRegister<EditProfileInputs>;
 }) {
+  const [socialLinks, setSocialLinks] = useState({})
   return (
     <InputWrap>
       <h3>Social Media</h3>
@@ -15,51 +17,52 @@ export default function SocialMedia({
         <FormControl
           as="input"
           type="text"
-          name="facebook"
           register={register}
           labelText="Facebook"
-          placeholder="your facebook handle"
+          placeholder="your facebook link"
+          onChange={(e) => setSocialLinks({ ...socialLinks, 'facebook': e.target.value })}
         />
         <FormControl
           as="input"
           type="text"
-          name="x"
           register={register}
           labelText="X"
-          placeholder="your X handle"
+          placeholder="your X link"
+          onChange={(e) => setSocialLinks({ ...socialLinks, 'x': e.target.value })}
         />
         <FormControl
           as="input"
           type="text"
-          name="linkedin"
           register={register}
           labelText="LinkedIn"
-          placeholder="your linkedin handle"
+          placeholder="your linkedin link"
+          onChange={(e) => setSocialLinks({ ...socialLinks, 'linkedin': e.target.value })}
         />
         <FormControl
           as="input"
           type="text"
-          name="instagram"
           register={register}
           labelText="Instagram"
-          placeholder="your instagram handle"
+          placeholder="your instagram link"
+          onChange={(e) => setSocialLinks({ ...socialLinks, 'instagram': e.target.value })}
         />
         <FormControl
           as="input"
           type="text"
-          name="youtube"
           register={register}
           labelText="Youtube"
-          placeholder="your youtube handle"
+          placeholder="your youtube link"
+          onChange={(e) => setSocialLinks({ ...socialLinks, 'youtube': e.target.value })}
         />
         <FormControl
           as="input"
           type="text"
-          name="tiktok"
           register={register}
           labelText="TikTok"
-          placeholder="your TikTok handle"
+          placeholder="your TikTok link"
+          onChange={(e) => setSocialLinks({ ...socialLinks, 'tiktok': e.target.value })}
         />
+        <input type="text" hidden name='socials' value={JSON.stringify(socialLinks)} />
       </fieldset>
     </InputWrap>
   );
