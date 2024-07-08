ddc014c6ca4a7c03798ec11d3613cd411d23e67e
Usirusen Ikon <lexicon048@gmail.com>
2024-07-08 12:41:55 +0100

    Refactor: Simplify navigation component logic

--- src/components/navigation/index.tsx
+++ src/components/navigation/index.tsx
@@ -29,8 +29,7 @@ function Navigation({ res }: { res: ErrorResProps }) {
   const [tooltip, setToolTip] = useState(false);
   const { navBar, goingUp, open, setOpen } = useInteractiveNav();
 
-  if (open === true || location.search.includes(`auth`))
-    document.body.style.overflowY = 'hidden';
+  if (open) document.body.style.overflowY = 'hidden';
   else document.body.style.overflowY = '';
 
   const onClickHandler = () => {
